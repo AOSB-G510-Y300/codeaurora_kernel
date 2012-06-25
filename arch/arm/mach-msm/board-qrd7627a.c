8be997b985b46b71a36b5fd2064605800e1f4b24
Benjamin Gaignard <benjamin.gaignard@linaro.org>
2012-06-25 15:27:30 -0700

    fix ion_platform_data definition
    fix ion_platform_heap to make is use an usual way
    in board configuration file.
    
    Change-Id: Ia5129a4c81bb6719f471214e75d77e51f5f50c59
    Signed-off-by: Benjamin Gaignard <benjamin.gaignard@linaro.org>
    [lauraa: Fixup msm board files]
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>
    [cpandya: clean up for checkpatch]
    Signed-off-by: Chintan Pandya <cpandya@codeaurora.org>
    
    Conflicts:
    	arch/arm/mach-msm/board-msm7x27a.c
    	arch/arm/mach-msm/board-qrd7627a.c

--- arch/arm/mach-msm/board-qrd7627a.c
+++ arch/arm/mach-msm/board-qrd7627a.c
@@ -695,8 +695,7 @@ static struct ion_co_heap_pdata co_ion_pdata = {
  * These heaps are listed in the order they will be allocated.
  * Don't swap the order unless you know what you are doing!
  */
-
-struct ion_platform_heap msm7x27a_heap[] = {
+struct ion_platform_heap msm7627a_heaps[] = {
 		{
 			.id	= ION_SYSTEM_HEAP_ID,
 			.type	= ION_HEAP_TYPE_SYSTEM,
@@ -730,14 +729,10 @@ struct ion_platform_heap msm7x27a_heap[] = {
 #endif
 };
 
-/**
- * These heaps are listed in the order they will be allocated.
- * Don't swap the order unless you know what you are doing!
- */
 static struct ion_platform_data ion_pdata = {
 	.nr = MSM_ION_HEAP_NUM,
 	.has_outer_cache = 1,
-	.heaps = msm7x27a_heap,
+	.heaps = msm7627a_heaps,
 };
 
 static struct platform_device ion_dev = {
@@ -1039,8 +1034,7 @@ static void __init msm_qrd_init(void)
 	msm_fb_add_devices();
 
 #if defined(CONFIG_BT) && defined(CONFIG_MARIMBA_CORE)
-    /* qcomm QRD phone concerned not need by hw product*/
-	//msm7627a_bt_power_init();
+	msm7627a_bt_power_init();
 #endif
 
 	msm7627a_camera_init();
